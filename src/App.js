8a02f1e289cef44934ff243129ac65c7b11041e1
musasazib <musasazib@gmail.com>
2021-09-22 13:03:26 +0600

    React Core Concept Recap and Practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookCount></BookCount>
+      <Posts></Posts>
     </div>
   );
+}
+
+function BookCount() {
+  const [book, setBook] = useState(0);
+  const addBook = () => setBook(book + 5);
+  const giftBook = () => setBook(book - 5);
+  return (
+    <div>
+      <h2>Book: {book}</h2>
+      <button onClick={addBook}>New Book</button>
+      <button onClick={giftBook}>Gift Book</button>
+    </div>
+  )
+}
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+  return (
+    <div>
+      {
+        posts.map(post => <Show title={post.title} body={post.body} id={post.id}></Show>)
+      }
+    </div>
+  )
+}
+function Show(props) {
+  return (
+    <div className="style">
+      <h3>{props.id}</h3>
+      <h2>{props.title}</h2>
+      <h4>{props.body}</h4>
+    </div>
+  )
 }
 
 export default App;
